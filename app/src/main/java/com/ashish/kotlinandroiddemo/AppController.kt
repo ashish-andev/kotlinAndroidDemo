0b6fee4d555f2b81aca986de1f52f6c7abd6e1de
ashishkdataback <ashish.k@databackapp.com>
2017-11-21 11:49:14 +0530

    intial commit

---
+++ app/src/main/java/com/ashish/kotlinandroiddemo/AppController.kt
@@ -0,0 +1,15 @@
+package com.ashish.kotlinandroiddemo
+
+import android.app.Application
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by digismart on 9/11/17.
+ */
+class AppController : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/ashish/kotlinandroiddemo/MainActivity.kt
+++ app/src/main/java/com/ashish/kotlinandroiddemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ashish.kotlinandroiddemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ashish.kotlinandroiddemo.R
 
 class MainActivity : AppCompatActivity() {
 
